058fba15021e1c731b15a7129cf8160b1bea4e9c
Monika Sharma <monicaa.shrama@gmailcom>
2026-06-11 18:32:02 +0530

    Fix production CORS

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 // MIDDLEWARE
 // =====================
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL ,
   credentials: true,
 }));
 
